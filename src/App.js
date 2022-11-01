665969bb9224cf8765538d51fb33f6363d8f3213
satishlakamsani <lakamsanisatish@gmail.com>
2022-11-01 04:42:40 +0000

    Rock paper Scissor

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import PlayingView from './PlayingView'
+import './App.css'
+
+const choicesList = [
+  {
+    id: 'ROCK',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rock-image.png',
+  },
+  {
+    id: 'SCISSORS',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/scissor-image.png',
+  },
+  {
+    id: 'PAPER',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/paper-image.png',
+  },
+]
+
+const App = () => <PlayingView choicesList={choicesList} />
+export default App
